59934ff98d3502279ba5e9b0b4e3667d86632afa
jorgegm-eng <jorgegmmgg@gmail.com>
2026-03-06 07:55:19 +0100

    feat: implement match analysis service logic

--- SummonerScope.Application/Interfaces/IPlayerService.cs
+++ SummonerScope.Application/Interfaces/IPlayerService.cs
@@ -6,5 +6,5 @@ public interface IPlayerService
 {
     Task<PlayerDto?> GetPlayerAsync(string region, string gameName, string tagLine);
     Task<PlayerMatchesDto?> GetPlayerMatchesAsync(string region, string gameName, string tagLine, int count = 10);
-
+    Task<MatchAnalysisDto?> GetMatchAnalysisAsync(string matchId, string puuid);
 }
