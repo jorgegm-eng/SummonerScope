d31d453498d6be5182d237a5f3d691428440b6cc
jorge <jorgegmmgg@gmail.com>
2026-03-06 14:08:00 +0100

    feat: register memory cache

--- SummonerScope.API/Program.cs
+++ SummonerScope.API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
